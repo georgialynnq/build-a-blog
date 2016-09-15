38d5bd7ff28a7bc79fc1414b2bf3ff7380dc5201
Georgia Quinones <georgialynnq@gmail.com>
2016-09-15 18:33:40 -0500

    part one complete

--- main.py
+++ main.py
@@ -14,12 +14,65 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class NewPost(db.Model):
+    title = db.StringProperty(required = True)
+    blog = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+    def render_front(self, title="", blog="", error=""):
+        blogs = db.GqlQuery("SELECT * FROM NewPost "
+                            "ORDER by created DESC "
+                            "LIMIT 5")
+
+        self.render("front.html", title=title, blog=blog, error=error, blogs=blogs)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        blog = self.request.get("blog")
+
+        if title and blog:
+            a = NewPost(title = title, blog = blog)
+            a.put()
+
+            self.redirect("/blog")
+        else:
+            error = "Dont forget to include both the body and title!"
+            self.render_front(title, blog, error)
+
+class BlogHandler(Handler):
+    def render_recent(self, title="", blog=""):
+        blogs = db.GqlQuery("SELECT * FROM NewPost "
+                            "ORDER by created DESC "
+                            "LIMIT 5")
+
+        self.render("recent.html", title=title, blog=blog, blogs=blogs)
+
+    def get(self):
+        self.render_recent()
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+    ('/newpost', MainPage), ('/blog', BlogHandler)], debug=True)
